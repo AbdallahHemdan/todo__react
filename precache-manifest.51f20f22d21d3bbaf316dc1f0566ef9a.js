c644bc74cc041e24ebcc9b5792671c1b57382f84
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-02-22 16:00:24 +0200

    Updates

---
+++ precache-manifest.51f20f22d21d3bbaf316dc1f0566ef9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fda9c2d51e0be986cf745d08214ad71",
+    "url": "/ToDo-With-React/index.html"
+  },
+  {
+    "revision": "b313b1e20adf715f78b9",
+    "url": "/ToDo-With-React/static/css/main.88c7d111.chunk.css"
+  },
+  {
+    "revision": "53c679e4a8eeb3b261bb",
+    "url": "/ToDo-With-React/static/js/2.28d494b8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ToDo-With-React/static/js/2.28d494b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b313b1e20adf715f78b9",
+    "url": "/ToDo-With-React/static/js/main.56dd3d9d.chunk.js"
+  },
+  {
+    "revision": "73ca540174abe005d9b0",
+    "url": "/ToDo-With-React/static/js/runtime-main.289d5508.js"
+  }
+]);
